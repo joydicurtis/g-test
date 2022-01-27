83bce52b78956acecbb0ef0feb7f63f1833dd3ef
Yevheniia Nezhalska <Yevheniia.Nezhalska@teaminternational.com>
2022-01-28 00:41:04 +0200

    second commit

--- src/modules/board.component.js
+++ src/modules/board.component.js
@@ -7,14 +7,20 @@ export class BoardComponent {
 function getElementInViewPort() {
     const boardBalloon = document.querySelector('#js-board-balloon');
     const boardBalloonCats = document.querySelector('#js-board-balloon-cats');
+    const cloudLeft = document.querySelector('#js-cloud-left');
+    const cloudRight = document.querySelector('#js-cloud-right');
+    const clock = document.querySelector('#js-clock');
     toggleAnimation(boardBalloon, 'bounce-in-left');
     toggleAnimation(boardBalloonCats, 'bounce-in-right');
+    toggleAnimation(cloudLeft, 'bounce-in-left');
+    toggleAnimation(cloudRight, 'bounce-in-right');
+    toggleAnimation(clock, 'bounce-in-left');
 }
 
 function toggleAnimation(el, className) {
     const windowHeight = window.innerHeight;
     const elTop = el.getBoundingClientRect().top;
-    const elementVisible = 150;
+    const elementVisible = 0;
     if (elTop < windowHeight - elementVisible) {
       el.classList.remove('u-hidden');
       el.classList.add(className);
